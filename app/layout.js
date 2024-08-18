4f2ef838e96a7b80df2b74ee426289e14ba6a9d3
Barrington Patterson <barry545young@gmail.com>
2024-08-18 17:48:12 -0500

    "Updated Nav component with icons and links, added styles to globals.css, and modified layout.js and tailwind.config.js"

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <div className="flex flex-col h-screen max-h-screen">
           <Nav />
-          <div className="flex-grow overflow-y-auto bg-page text-default">
+          <div className="flex-grow overflow-y-auto bg-page text-default-text">
             {" "}
             {children}
           </div>
